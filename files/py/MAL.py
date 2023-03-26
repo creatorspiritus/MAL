d94f900d76b9ba5d97d1eacef8a6996ed676361a
CreatorSpiritus <mk@kuz.ru>
2023-03-26 14:25:36 +0300

    Добавлено свойство ИД

---
+++ files/py/MAL.py
@@ -0,0 +1,24 @@
+﻿import configparser
+class Я:
+    def __init__(я, *СЧ, **РЧ):
+        я.__СЧ = СЧ
+        я.__РЧ = РЧ
+
+    @property
+    def СЧ(я): return я.__СЧ
+
+    @property
+    def РЧ(я): return я.__РЧ
+
+    @property
+    def ИД(я):
+        возврат = configparser.ConfigParser()
+        try:
+            файл = я.РЧ.get('ini', '../ini/Я.ini')
+            возврат.read(файл, encoding='utf8')
+        except: возврат = {}
+        finally: return возврат
+
+if __name__ == '__main__':
+    МАЛ = Я(Фамилия = "Котляров", ini = '../ini/МАЛ.ini')
+    print(МАЛ.ИД.sections())
