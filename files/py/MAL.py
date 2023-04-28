957ae56584d7cb19518844fbab07b4fd977e8564
CreatorSpiritus <mk@kuz.ru>
2023-04-28 13:01:13 +0300

    Обновление MAL.py
    Оптимизация кода класса А

--- files/py/MAL.py
+++ files/py/MAL.py
@@ -5,6 +5,8 @@ from collections import namedtuple
 from configparser import ConfigParser
 from datetime import timedelta, time, datetime
 
+КТА = namedtuple('КТА', 'широта долгота превышение')
+
 class Я:
 	def __init__(я, *СЧ, **РЧ):
 		я.__СЧ = СЧ
@@ -30,17 +32,19 @@ class ИД(Я):
 class А(Я):
 	'''
 	Класс АЭРОДРОМ
-	При создании экземпляра класса требуется
-	обязательный ввод значения кода ИКАО аэродрома
+	При создании экземпляра класса 
+	требуется обязательный ввод 
+	значения кода ИКАО аэродрома
 	Пример: NOZ = А('UNWW')
 	'''
 	def __init__(я, *СЧ, **РЧ):
 		_РЧ = РЧ
 		_СЧ = []
 		f = pd.read_csv('../csv/aopa-points-export.csv', sep=';', index_col='Индекс')
+		_РЧ['ICAO'] = СЧ[0].upper()
 		for _ in f.columns:
 			_РЧ[_] = РЧ.get(_, f.loc[СЧ[0].upper()][_])
-		f = pd.read_csv('../csv/СА1.csv', index_col='ICAO')
+		f = pd.read_csv('../csv/СА.csv', index_col='ICAO')
 		for _ in f.columns:
 			_РЧ[_] = РЧ.get(_, f.loc[СЧ[0].upper()][_])
 		f = pd.read_csv('../csv/FTD.csv', index_col='FT')
@@ -50,6 +54,71 @@ class А(Я):
 					_СЧ.append(_[4:])
 		super().__init__(*_СЧ, **_РЧ)
 		del _РЧ, f, _СЧ
+	
+	@property
+	def УА(я):
+		if я.РЧ['Вид'] == 'УА': return True
+		else: return False
+	
+	@property
+	def АН(я):
+		if я.РЧ['Вид'] == 'АН': return True
+		else: return False
+	
+	@property
+	def КТА(я):
+		'''
+		СВОЙСТВО
+		Характеристика контрольной точки
+		аэродрома
+		ТВЗ: namedtuple КТА
+		'''
+		return КТА(
+			я.РЧ['Широта КТА'],
+			я.РЧ['Долгота КТА'],
+			я.РЧ['Превышение'])
+			
+	@property
+	def НВ(я):
+		'''
+		СВОЙСТВО
+		Все возможные направления полётов
+		ТВЗ: list
+		'''
+		возврат = []
+		for _ in я.СЧ:
+			if isinstance(_, str) and (len(_) == 4): возврат.append(_)
+		return возврат
+	
+	@property
+	def НП(я):
+		'''
+		СВОЙСТВО
+		Направления полётов с учётом 
+		категории аэропортов, даты ввода 
+		в эксплуатацию ближайших опорных 
+		аэродромов, типов ВС
+		'''
+		возврат = []
+		return возврат
+	
+	@property
+	def ОА(я):
+		'''
+		СВОЙСТВО
+		Возвращает именованный кортеж
+		с информацией о порядковом
+		номере опорного аэродрома,
+		дате ввода в эксплуатацию или
+		None
+		'''
+		возврат = False
+		for _ in ИД().РЧ['Опорные аэродромы']:
+			if я.РЧ['ICAO'].lower() == _.lower():
+				возврат = True
+			else: ...
+		return возврат
+
 	
 	def __repr__(я):
 		возврат = ''
