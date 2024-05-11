fa0477f1c8d439da7cdc04e85401bc30c21e047e
CreatorSpiritus <mk@kuz.ru>
2024-05-11 10:52:27 +0300

    Подготовлены таблицы городов для каждого аэродрома проекта МАЛ

---
+++ files/py/классы.py
@@ -0,0 +1,44 @@
+from sqlmodel import SQLModel, Field
+from collections import namedtuple
+
+К = namedtuple('КТА', 'широта долгота превышение')
+
+# class Город(SQLModel):
+#     """
+#     Класс "Город" описывает данные населённых пунктов
+#     """
+#     id: int | None = Field(default=None, primary_key=True)
+#     регион: str | None
+#     район: str | None
+#     город: str | None
+#     жителей: int | None
+#     широта: float | None
+#     долгота: float | None
+
+
+class Аэродром(SQLModel):
+    """
+    Класс "Аэродром" описывает данные аэродрома
+    """
+    id: int | None = Field(default=None, primary_key=True)
+    IATA: str | None = Field(max_length=3)
+    ICAO: str | None = Field(max_length=4)
+    город: str | None
+    название: int | None
+    широта: float | None
+    долгота: float | None
+
+
+
+class Муниципалитет(SQLModel):
+    """
+    Класс "Муниципалитет" описывает данные населённого пункта
+    """
+    id: int | None = Field(default=None, primary_key=True)
+    регион: str | None
+    район: int | None
+    название: float | None
+    тип: float | None
+    широта: float | None
+    долгота: float | None
+    население: int | None
