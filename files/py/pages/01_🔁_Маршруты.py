b5be75a7208ef64084b36b6619fc19301518d818
CreatorSpiritus <mk@kuz.ru>
2024-05-25 05:38:13 +0300

    Добавлены флаги аэродромов

--- files/py/pages/01_🔁_Маршруты.py
+++ files/py/pages/01_🔁_Маршруты.py
@@ -1,4 +1,22 @@
 import streamlit as st
+from pandas import read_csv, DataFrame
+from функции import Наименование, наименования
 
 st.title("Маршруты")
 st.divider()
+st.session_state.keys()
+a = []
+# for _ in st.session_state.keys():
+#     if (len(_) == 4) and st.session_state[_]:
+#         a.append(Наименование(_))    
+
+# a = наименования()
+# 
+вылет = st.sidebar.selectbox("Вылет", a)
+# 
+# файл = '../csv/Аэродромы/' + вылет[:4] + '_1000_перечень_аэродромов.csv'
+# d = read_csv(файл, index_col='ICAO')
+# b = [Наименование(_) for _ in d.index]
+# 
+# 
+# прилёт = st.sidebar.selectbox("Прилёт", b.remove(вылет))
