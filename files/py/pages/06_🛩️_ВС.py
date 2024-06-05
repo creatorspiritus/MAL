907bce40c087ef16bc4c48cc82cb13de3bb98356
CreatorSpiritus <mk@kuz.ru>
2024-06-05 16:42:28 +0300

    Реорганизация структуры

--- files/py/pages/06_🛩️_ВС.py
+++ files/py/pages/06_🛩️_ВС.py
@@ -2,3 +2,13 @@ import streamlit as st
 
 st.title("Самолёты")
 st.divider()
+
+st.session_state['самолёты_проекта'] = st.multiselect(
+    'Самолёты проекта', 
+    st.session_state['самолёты'],
+    default=[
+        "Ил-114-300",
+        "ТВРС-44 Ладога",
+        "ЛМС-192 Освей",
+        "ЛМС-901 Байкал"
+    ])
