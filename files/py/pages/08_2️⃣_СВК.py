2d8de9162102c2d26fd69940ef5b2975e12eb509
CreatorSpiritus <mk@kuz.ru>
2024-06-06 09:03:08 +0300

    Добавлены значения переменных сессии

--- files/py/pages/08_2️⃣_СВК.py
+++ files/py/pages/08_2️⃣_СВК.py
@@ -1,4 +1,13 @@
 import streamlit as st
+from перечни import перечень_СВК
 
 st.title("II Этап | Северный воздушный коридор")
 st.divider()
+
+if "СВКВ" not in st.session_state:
+    st.session_state["СВКВ"] = st.session_state["СВК"]
+
+st.session_state["СВКВ"] = st.multiselect(
+    "Аэропорты северного воздушного коридора",
+    default=st.session_state['СВК'],
+    options=перечень_СВК)
